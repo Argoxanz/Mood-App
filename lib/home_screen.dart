f470f21c143f3d5996ccd8e26743dd1019ceee18
Argoxanz <147998192+Argoxanz@users.noreply.github.com>
2024-07-04 21:35:14 +0500

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -41,6 +41,7 @@ class HomeScreen extends StatelessWidget {
             ),
           )
         ],
+        centerTitle: true,
         title: Text(
           "${now.day} ${months[now.month - 1]} ${now.hour.toString().padLeft(2, "0")}:${now.minute.toString().padLeft(2, "0")}",
           style: const TextStyle(fontWeight: FontWeight.bold, color: grey2),
